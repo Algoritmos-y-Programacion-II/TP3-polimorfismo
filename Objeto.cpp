8ffce2990727fb299389fa1acc55d7f778101946
Valentina Varela Rodriguez <vavarela.rodriguez@gmail.com>
2020-06-08 19:43:47 -0300

    Agregue clase Objeto

---
+++ Objeto.cpp
@@ -0,0 +1,5 @@
+//
+// Created by valentina on 8/6/20.
+//
+
+#include "Objeto.h"
---
+++ Objeto.h
@@ -0,0 +1,17 @@
+//
+// Created by valentina on 8/6/20.
+//
+
+#ifndef TRABAJOPRACTICO3FIGURAS_OBJETO_H
+#define TRABAJOPRACTICO3FIGURAS_OBJETO_H
+
+#include "Validaciones.h"
+
+class Objeto {
+
+    protected:
+        Validaciones validacion;
+};
+
+
+#endif //TRABAJOPRACTICO3FIGURAS_OBJETO_H
