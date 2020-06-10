3fbc6560d60bdb74e2d8cfd1e0028348a6daf41d
Valentina Varela Rodriguez <vavarela.rodriguez@gmail.com>
2020-06-10 00:42:51 -0300

    Agregue comentarios

--- Archivo.h
+++ Archivo.h
@@ -9,7 +9,6 @@
 #include "Lista.h"
 
 using namespace std;
-
 typedef Figura* Dato;
 
 class ArchivoLectura {
@@ -18,16 +17,42 @@ class ArchivoLectura {
         ifstream archivo;
 
     public:
-        ArchivoLectura(string ruta);
-        ~ArchivoLectura();
-        void cerrarArchivo();
-        char leerCaracter();
-        double leerReal();
-        bool abierto();
-        bool finalArchivo();
-        bool existe(string ruta);
-        void cargarDatos(Lista<Dato> &lista);
 
+        // PRE: ruta es un string valido
+        // POST:
+        ArchivoLectura(string ruta);
+
+        // PRE: El archivo fue abierto
+        // POST: Cierra el archivo
+        ~ArchivoLectura();
+
+        // PRE: El archivo fue abierto
+        // POST: Cierra el archivo
+        void cerrarArchivo();
+
+        // PRE: El archivo fue abierto, el dato a leer debe ser un caracter
+        // POST: Lee un dato del archivo como caracter y lo devuelve
+        char leerCaracter();
+
+        // PRE: El archivo fue abierto, el dato a leer debe ser un numero
+        // POST: Lee un dato del archivo como double y lo devuelve
+        double leerReal();
+
+        // PRE: -
+        // POST: Devuelve true si el archivo esta abierto correctamente
+        bool abierto();
+
+        // PRE: El archivo fue abierto
+        // POST: Devuelve true si se llego al final del archivo
+        bool finalArchivo();
+
+        // PRE: ruta es un string valido
+        // POST: Devuelve true si el archivo existe
+        bool existe(string ruta);
+
+        // PRE: La lista es válida
+        // POST: Carga los datos del archivo en la lista
+        void cargarDatos(Lista<Dato> &lista);
 };
 
 
